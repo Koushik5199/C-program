0d7f32892249c183ba40331e7381dbc0efcfcfd0
SATYA PRANEETH KOUSHIK SRIPADA <praneethk9912@gmail.com>
2025-11-02 09:54:45 +0530

    Add files via upload

---
+++ Hello_World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//To write a simple Hello World !! Program in C
+
+int  main ()
+{
+
+printf("Hello World !! This is a sample C program");
+
+return 0;
+}
