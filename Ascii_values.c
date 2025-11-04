9364f3b18aa2251dfedb10aa122dea16f24a6a44
SATYA PRANEETH KOUSHIK SRIPADA <praneethk9912@gmail.com>
2025-11-04 22:54:57 +0530

    Add files via upload

---
+++ Ascii_values.c
@@ -0,0 +1,29 @@
+/*
+Write a program to find the ASCII Value of a charachter.
+
+Note : Ascii value of 'A' start at 65 and ends at 'Z' i.e; 90
+         similarly a = 97 and z =122
+         
+*/
+
+#include<stdio.h>
+int main()
+{
+    char ASCII_VALUE='a';
+
+    
+    printf("%d",ASCII_VALUE);
+    
+    return 0;
+    
+}
+
+/*
+
+scan using its data type like %c
+print using %d
+
+*/
+
+
+
